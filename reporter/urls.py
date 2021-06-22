0ed7b50a8bdbc89bf6b33c62ef253d9867cf0430
junghong <junghong0512@gmail.com>
2021-06-22 11:10:52 +0900

    Tutorial 5-1, Map on Home

--- reporter/urls.py
+++ reporter/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include, url
+from django.urls import re_path
 
-from .views import HomePageView
+from reporter.views import HomePageView
 
 urlpatterns = [
-    
+    re_path(r'^$', HomePageView.as_view(), name = 'home')
 ]
